580204acff9c6bbd5e4431d19c9c907f43ae632d
emmagsmith <84419055+emma-g-smith@users.noreply.github.com>
2024-10-07 15:44:43 -0700

    Ending changes

--- Assets/Scripts/DialogueHandler.cs
+++ Assets/Scripts/DialogueHandler.cs
@@ -45,13 +45,13 @@ public class DialogueHandler : MonoBehaviour
         dialogueInformations = new Dictionary<Dialogues, DialogueInformation>();
         dialogueInformations[Dialogues.Intro] = new DialogueInformation(new string[] { 
             "Witch: You are always so forgetful. Your controls are: [WASD] to move [e] to interact/get hints, and [123] to swap",
-            "Witch: sigh...",
-            "Witch: Being immortal is so lonely. The last time i saw my husband, he asked me to go see the world for him.",
-            "Witch: He knew he’d be gone by the time I returned. He had grown quite old. It was his last wish.", 
+            "Witch: sigh... Being immortal is so lonely.",
+            "Witch: The last time I saw my husband, he asked me to go see the world for him.",
+            "Witch: He knew he’d be gone by the time I returned, but it was his last wish.", 
             "Witch: I would give anything to see him again. My husband… where is he?"
         });
         dialogueInformations[Dialogues.MeetingMouse] = new DialogueInformation(new string[] {
-            "Mouse: Thanks for the assist! I have been stuck in that pumpking for a while now.",
+            "Mouse: Thanks for the assist! I have been stuck in that pumpkin for a while now.",
             "Mouse: How about I tag along with you?",
             "You: I wouldn't mind the company."
         });
@@ -79,7 +79,7 @@ public class DialogueHandler : MonoBehaviour
         }, noFill: true);
         dialogueInformations[Dialogues.Final] = new DialogueInformation(new string[] {
             "Witch: You found him? Words cannot express my gratitude!",
-            "Witch: Oh, how i’ve missed you!"
+            "Witch: Oh, how I’ve missed you!"
         });
 
 
